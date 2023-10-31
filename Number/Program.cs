408b623a18d0f3764b693f8e9d61f493b11a0b8a
Danh Phan <phandanh2112@gmail.com>
2023-10-31 20:28:39 +0700

    Number - ver 1.0 - init

---
+++ Number/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Method {
+    class Program {
+        /* ---------------------------------------- Main ---------------------------------------- */
+        static void Main(string[] args) {
+
+            Console.Write("Enter the number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Factorial of " + n + ": " + Factorial(n));
+
+            Console.WriteLine("The sum of all odd numbers from 1 to " + n + ": " + sumOdd(n));
+
+            if (isEven(n))
+                Console.WriteLine(n + " is an even number");
+            else
+                Console.WriteLine(n + " is an odd number");
+
+            // int a = int.Parse(Console.ReadLine());
+            // int b = int.Parse(Console.ReadLine());
+            // int c = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Max: " + Max3(a, b, c));
+        }
+
+
+
+        /* ---------------------------------------- APIs ---------------------------------------- */
+        public static bool isEven(int n) {
+            if (n % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static int Factorial(int n) {
+            if (n == 1 || n == 0) 
+                return 1;
+            return n * Factorial(n - 1);
+        }
+
+        public static int sumOdd (int n) {
+            if (n == 1) 
+                return 1;
+            if (n % 2 == 0)
+                return sumOdd(n-1);
+	        else
+                return n + sumOdd(n - 1);
+        }
+
+        public static int Max3(int a, int b, int c) {
+            int max = a;
+            if (b > max)
+                max = b;
+            if (c > max)
+                max = c;
+            return max;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CSharp_Basic
+# Description
+    Small C# programs handle different tasks by conceptual data types.
+
+# Software
+    Microsoft Visual Studio (or other IDEs are able to compile C#).
+
+# Source
+    - Number: problems related to array.
